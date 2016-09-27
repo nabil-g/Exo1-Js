b5fdcfbb7a7cf63dc124ed8a8af937c79a7093e3
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-09-27 14:31:14 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-/* votre fonction ici*/
+	
+	/* votre fonction ici*/
+
+var paragraphe = document.querySelector('p');
+var bouton = document.querySelector('button');
+
+//bouton.onclick = function() {
+//	changerStyle(paragraphe);
+//}
+
+function changerStyle(element) {
+	element.style.color = 'white';
+	element.style.backgroundColor = 'black';
+	element.style.border = '10px dotted red';
+	element.style.padding = '5px';
+};
+
+
